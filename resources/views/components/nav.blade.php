46cda40d91192c21725f8329b6526d5aca777e24
Emmanuel Iwok <iwokemmanuel49@gmail.com>
2024-05-10 20:15:19 +0100

    first project commit

---
+++ resources/views/components/nav.blade.php
@@ -0,0 +1,77 @@
+     <!-- Start::main-sidebar -->
+     <div class="main-sidebar" id="sidebar-scroll">
+
+        <!-- Start::nav -->
+        <nav class="main-menu-container nav nav-pills flex-column sub-open">
+            <div class="slide-left" id="slide-left">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="#7b8191" width="24" height="24" viewbox="0 0 24 24"> <path d="M13.293 6.293 7.586 12l5.707 5.707 1.414-1.414L10.414 12l4.293-4.293z"></path> </svg>
+            </div>
+            <ul class="main-menu">
+                <!-- Start::slide__category -->
+                <li class="slide__category"><span class="category-name">Main</span></li>
+                <!-- End::slide__category -->
+
+
+
+
+
+                <!-- Start::slide -->
+                <li class="slide ">
+                    <a href="javascript:void(0);" class="side-menu__item">
+                        <i class="bx bx-file-blank side-menu__icon"></i>
+                        <span class="side-menu__label">Profile<span class="badge bg-secondary-transparent ms-2">New</span></span>
+                        <i class="fe fe-chevron-right side-menu__angle"></i>
+                    </a>
+
+                </li>
+                <!-- End::slide -->
+
+                <!-- Start::slide -->
+                <li class="slide ">
+                    <a href="/" class="side-menu__item">
+                        <i class="bx bx-task side-menu__icon"></i>
+                        <span class="side-menu__label">Task<span class="badge bg-secondary-transparent ms-2">New</span></span>
+                        <i class="fe fe-chevron-right side-menu__angle"></i>
+                    </a>
+                                </li>
+                <!-- End::slide -->
+
+                <!-- Start::slide -->
+                <li class="slide ">
+                    <a href="javascript:void(0);" class="side-menu__item">
+                        <i class="bx bx-fingerprint side-menu__icon"></i>
+                        <span class="side-menu__label">Authentication</span>
+                        <i class="fe fe-chevron-right side-menu__angle"></i>
+                    </a>
+
+                </li>
+                <!-- End::slide -->
+
+                <!-- Start::slide -->
+                <li class="slide ">
+                    <a href="javascript:void(0);" class="side-menu__item">
+                        <i class="bx bx-error side-menu__icon"></i>
+                        <span class="side-menu__label">Error</span>
+                        <i class="fe fe-chevron-right side-menu__angle"></i>
+                    </a>
+
+                </li>
+                <!-- End::slide -->
+
+
+
+
+
+
+
+
+
+
+
+
+            </ul>
+        </nav>
+        <!-- End::nav -->
+
+    </div>
+    <!-- End::main-sidebar -->
