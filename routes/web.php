f054db040a5ed0de3c6e6fa3f4f6bd62124ffbd9
dike <dikewisdom787@gmail.com>
2024-05-12 15:40:33 +0100

    chore: Add Laravel Sanctum package for API authentication

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\CheckUserIsAdmin;
 
 Route::get('/', function () {
     return view('pages.index-1');
-});
+})->middleware(['admin']);
